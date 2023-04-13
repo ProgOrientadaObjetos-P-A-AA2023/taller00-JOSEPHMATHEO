5de112aa0ced450ae44705511345b750d181e1ae
JOSEPHMATHEO <91102368+JOSEPHMATHEO@users.noreply.github.com>
2023-04-13 11:48:15 -0500

    New Changes

--- Demo/src/paquete3/Valor.java
+++ Demo/src/paquete3/Valor.java
@@ -1,17 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete3;
+import paquete5.Dato;
 
-/**
- *
- * @author reroes
- */
 public class Valor {
     
     public static int obtenerHabitantes(){
+        
         return Dato.obtenerNumeroHombres() + Dato.obtenerNumeroMujeres();
+        
     }
+    
 }
